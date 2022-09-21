c11c558567e1c823a629bc54d9c8c16dacba4cfa
Aman dwivedi <111753149+amandwivedi70@users.noreply.github.com>
2022-09-21 23:30:30 +0530

    Update practice_assign_6.py

--- practice_assign_6.py
+++ practice_assign_6.py
@@ -104,8 +104,10 @@ else:
 month = int(input("enter your number"))
 if month in (1,3,5,7,8,10,12):
     print ("31 days")
+elif month in (4,6,9,11):
+    print("30 days")
 elif month==2:
-    print("30days")
+    print("28 or 29 days")
 else:
     print("invalid month number")
 
